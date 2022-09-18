5fb78073bcbe6ac43f30dcc7d7028f8d1dba44bb
fangyongfu <yongfufang1996@163.com>
2022-09-18 18:38:38 +0800

    git测试

---
+++ src/main/java/com/fang/People.java
@@ -0,0 +1,22 @@
+package com.fang;
+
+public class People {
+    int age;
+    String address;
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
